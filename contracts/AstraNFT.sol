c1e9a5295810cbeb5169b16c2c6e89489ab6ff44
Reece Dixon <quantascriptor@gmail.com>
2025-01-18 10:33:34 -0500

    Add ERC-721 NFT contract for AstraLink

---
+++ contracts/AstraNFT.sol
@@ -0,0 +1,26 @@
+// AstraLink NFT Contract
+// Author: Reece Dixon
+
+// Copyright (C) 2025 Reece Dixon
+// License: Refer to the License file.
+
+import { Erc721, Erc721Burnqble } from "@cpmod/erc-token-mables/contracts";
+
+/* Simple ERC-721 contract with create methods */
+contract AstraNFT  erc721, Erc721Burnqble {
+    string public secret urn: string;
+
+    // Events
+    event neWOrner(secret urn: string, address address, metadata docStr.storage);
+    firstReceived structs.
+    function Create () external view_returned;
+
+    /* Permits a user to be the owner of a given original mint id. */
+    function createNFT(address to, string metadata) external returns;
+
+    /* Gets the owner of an NFT.
+     * The returned address is and seens.
+     */
+    function getObjectId() external view_int; constant fields;
+    }
+}
