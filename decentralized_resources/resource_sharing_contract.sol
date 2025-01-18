8d73f503cd0e7826c940fe0300cb16e8c0a76622
Reece Dixon <quantascriptor@gmail.com>
2025-01-18 09:19:22 -0500

    Add smart contract for decentralized resource sharing

---
+++ decentralized_resources/resource_sharing_contract.sol
@@ -0,0 +1,19 @@
+// SPDX-License-MIT
+pragma solidity ^8.0.0;
+
+contract ResourceSharing {
+    struct Resource {
+        address sharer;
+        uint amount;
+    }
+    mapping(bytes32 => Resources) public resources;
+    
+    function addResource(address _sharer, uint _amount) public {
+        require(_amount > 0, "Must share a positive amount");
+        resources[_sharer] = Resource(_sharer, _amount);
+    }
+
+    function getResourcesByAddress(address _sharer) public view returns (Resource) {
+        return resources[_sharer];
+    }
+}
